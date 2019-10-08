4c3cc5393bf45a1bdddedc1267e520fc94d9064b
Sang Uk Park <parksang1993@gmail.com>
2019-10-08 16:03:14 +0900

    final_version

--- acmip_1395.cpp
+++ acmip_1395.cpp
@@ -10,6 +10,9 @@ using namespace std;
 class SegmentTree{
 public:
     vector<int> tree;
+    vector<int> lazy;
+//    int tree[100000*2] = {0};
+//    int lazy[100000*2] = {0};
     int base = 0;
 
 public:
@@ -17,20 +20,13 @@ public:
     SegmentTree(int size){
         base = 1;
         while(base < size) base <<= 1;
-        tree.assign(base * 2 + 1, 0);
+        tree.resize(base * 2 + 1);
+        lazy.resize(base * 2 + 1);
     }
 
-    //각 노드들의 합을 구해주는 함수
-    void sum(){
-        int index = base - 1;
-        while(index >= 1){
-            tree[index] = tree[index * 2] + tree[index * 2 + 1];
-            index--;
-        }
-    }
-
-    long long quary(int targetL, int targetR, int curL, int curR, int index){
+    int quary(int targetL, int targetR, int curL, int curR, int index){
         if(curL > targetR || curR < targetL) return 0;
+        propagate(curL, curR, index);
         if( targetL <= curL && curR <= targetR) return tree[index];
         int mid = (curL + curR) >> 1;
 
@@ -38,30 +34,36 @@ public:
                + quary(targetL, targetR, mid + 1, curR, index * 2 + 1);
     }
 
-    void update(int start, int end){
 
-        start += base - 1;
-        for(int i=start; i<end+1; i++){
-            tree[i] = tree[i] == 1 ? 0 : 1;
-            i >>= 1;
-            while(i >= 1){
-                tree[i] = tree[i * 2] + tree[i * 2 + 1];
-                i >>= 1;
+    void propagate(int start, int end, int index){
+        if(lazy[index]){
+            if(start < end){
+                lazy[index * 2] = !lazy[index*2];
+                lazy[index * 2 + 1] = !lazy[index*2+1];
             }
+            tree[index] = (end - start) + 1 - tree[index];
+            lazy[index] = 0;
         }
     }
 
-    // 출력해주는 함수
-    void print(){
-        cout << "Segment Tree" << endl;
-        for(int i=1; i<base; i++){
-            printf("parent[%d]-%d\t", i, tree[i]);
+
+
+    void reverse(int start, int end, int curL, int curR, int index=1){
+        if(start > curR || end < curL) return;
+        propagate(curL, curR, index);
+
+        if(start <= curL && curR <= end){
+            tree[index] = end-start + 1 - tree[index];
+            if(curL!=curR){
+                lazy[index * 2] = !lazy[index*2];
+                lazy[index * 2 + 1] = !lazy[index*2+1];
+            }
+            return;
         }
-        cout << endl;
-        for(int i=base; i<tree.size(); i++){
-            printf("data[%d]-%d\t", i, tree[i]);
-        }
-        cout << endl;
+
+        int mid = (curL + curR) >> 1;
+        reverse(start, end, curL, mid, index*2);
+        reverse(start, end, mid+1, curR, index*2+1);
     }
 
 };
@@ -76,15 +78,11 @@ int main(){
 
     for(int i=0; i<M; i++){
         cin >> O >> num1 >> num2;
-        switch(O){
-            case 0:
-                tree.update(num1, num2);
-                tree.print();
-                break;
-            case 1:
-                cout << tree.quary(num1, num2, 1, N, 1) << endl;
-                break;
-        }
+        if(O)
+            cout << tree.quary(num1, num2, 1, N, 1) << endl;
+        else
+            tree.reverse(num1, num2, 1, N, 1);
+//
 
     }
     return 0;
